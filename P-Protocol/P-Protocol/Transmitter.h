9be604e1285c7461be4bc05f491e245db29c5565
Robin <r.diedrichs@web.de>
2017-03-03 18:11:14 +0100

    added basic structure for Transmitter.h

---
+++ P-Protocol/P-Protocol/Transmitter.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#ifdef _DEBUG
+
+#include "bcm2835.h"
+#define RPI_GPIO_P1_15 22
+#define BCM2835_GPIO_FSEL_INPT 0x00
+#define BCM2835_GPIO_FSEL_OUTP 0x01
+#include <time.h>
+
+#endif
+
+#ifndef _DEBUG
+#include <bcm2835.h>
+#endif
+
+#define INP 0
+#define OUP 1
+
+
+class Transmitter
+{
+public:
+	Transmitter(int pin, int mode)
+		:
+		pin(pin),
+		mode(mode)
+	{
+		if (!alr_init)
+		{
+			init_err = bcm2835_init();
+			alr_init = true;
+		}
+		if (!init_err)
+		{
+			bcm2835_gpio_fsel(pin, mode == INP ? BCM2835_GPIO_FSEL_INPT : BCM2835_GPIO_FSEL_OUTP);
+		}
+
+		amount++;
+	}
+	~Transmitter()
+	{
+		amount--;
+		if (amount == 0)
+		{
+			bcm2835_close();
+			alr_init = false;
+			init_err = false;
+		}
+	}
+	int read()
+	{
+		return bcm2835_gpio_lev(pin);
+	}
+	static void close()
+	{
+		if (!init_err)
+			bcm2835_close();
+	}
+	static void sleep(size_t time)
+	{
+		bcm2835_delay(time);
+	}
+
+private:
+	static bool init_err;
+	static bool alr_init;
+	static size_t amount;
+	int pin;
+	int mode;
+};
+
+size_t Transmitter::amount = 0;
+bool Transmitter::init_err = false;
+bool Transmitter::alr_init = false;
---
+++ P-Protocol/P-Protocol/bcm2835.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <stdlib.h>
+
+#ifdef _DEBUG
+#include <windows.h>
+#endif
+
+bool bcm2835_init()
+{
+	printf("init\n");
+	return true;
+}
+void bcm2835_close()
+{
+	printf("close\n");
+	return;
+}
+void bcm2835_gpio_fsel(int a, int b)
+{
+	return;
+}
+int bcm2835_gpio_lev(int a)
+{
+	if (static_cast<double>(rand()) / static_cast<double>(RAND_MAX) < 0.5)
+		return 0;
+	else
+		return 1;
+}
+void bcm2835_delay(int a)
+{
+	Sleep(a);
+}
