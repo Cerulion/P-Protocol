8b3e5768ff43aae608b19246b3161863d2eda72b
Robin <r.diedrichs@web.de>
2017-03-03 18:16:12 +0100

    fixed failure in last commit

--- P-Protocol/P-Protocol/bcm2835.h
+++ P-Protocol/P-Protocol/bcm2835.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdlib.h>
+#include <stdio.h>
 
 #ifdef _DEBUG
 #include <windows.h>
--- P-Protocol/P-Protocol/main.cpp
+++ P-Protocol/P-Protocol/main.cpp
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+#include "Transmitter.h"
+
+#define INPUT_PIN BCM2835_GPIO_FSEL_INPT
+
+
 int main()
 {
-	printf("haha\n");
+#ifdef _DEBUG
+	srand(time(NULL));
+#endif
 
+	Transmitter hans = Transmitter(INPUT_PIN, INP);
+
+	for (int i = 0; i < 20; i++)
+	{
+		printf("%d\n", hans.read());
+		Transmitter::sleep(25);
+	}
+
+	while (getchar() != 'q');
 	return 0;
 }
